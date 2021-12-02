2a7eeaf0887b6af850eacc9adef9364fda4d3cf6
Anderson Freires <dre.son36@gmail.com>
2021-12-02 10:54:30 -0200

    Saving and deleting in the database

--- mysoccer/src/main/java/aplicacao/Programa.java
+++ mysoccer/src/main/java/aplicacao/Programa.java
@@ -1,16 +1,40 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Clube;
 
 public class Programa {
  public static void main(String[] args) {
-	Clube c1= new Clube(1,"Palmeiras", "Allianz Park");
-	Clube c2= new Clube(2,"Grêmio", "Arena do Grêmio");
-	Clube c3= new Clube(3,"Atlêtico Paranaense", "Arena da Baixada");
+	Clube c1= new Clube(null,"Palmeiras", "Allianz Park");
+	Clube c2= new Clube(null,"Grêmio", "Arena do Grêmio");
+	Clube c3= new Clube(null,"Atlêtico Paranaense", "Arena da Baixada");
 	
-	System.out.println(c1);
-	System.out.println(c2);
-	System.out.println(c3);
+	EntityManagerFactory emf= Persistence.createEntityManagerFactory("mysoccerclubs");
+	EntityManager em=emf.createEntityManager(); 
 	
+	Clube c=em.find(Clube.class,3);
+	
+	em.getTransaction().begin();
+	em.remove(c);
+	em.getTransaction().commit();
+	
+	System.out.println(c);
+	
+	
+	
+	/*
+	em.getTransaction().begin();
+	em.persist(c1);
+	em.persist(c2);
+	em.persist(c3);
+	em.getTransaction().commit();
+	*/
+	
+	System.out.println("Pronto");
+	em.close();
+	emf.close();
  }
 }
